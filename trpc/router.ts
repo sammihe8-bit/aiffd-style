b97db12c40cbe9ac63436914c8641025401f13a2
sammihe8-bit <sammihe8@gmail.com>
2026-05-05 11:44:12 -0700

    Remove superjson transformer from tRPC router

--- trpc/router.ts
+++ trpc/router.ts
@@ -1,6 +1,5 @@
 import { initTRPC } from "@trpc/server";
 import { z } from "zod";
-import superjson from "superjson";
 import { db } from "../db";
 import { users, styleDiagnoses, styleSystems, subscriptions } from "../db/schema";
 import { eq, desc } from "drizzle-orm";
@@ -8,7 +7,6 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const t = initTRPC.create({
-  transformer: superjson,
 });
 
 export const router = t.router;
